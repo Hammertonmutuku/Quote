e67794808d4b461b1e0bb50a22006181f99354ff
Hammertonmutuku <lthamma3@gmail.com>
2019-12-04 06:07:38 +0300

    added the quote class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote }from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote [] = [
+    new  Quote (1,"Hammerton", "The best is yet to come",0,0),
+    new Quote (2,"Simon kimani", "Get ready for change",0,0),
+    new Quote (3,"Kelvin Mbugua","Change is inevitable",0,0),
+  ];
 
   constructor() { }
 
