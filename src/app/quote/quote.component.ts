7a5a1287455283d6bb238ad04bfe6ae006f6e176
Hammertonmutuku <lthamma3@gmail.com>
2019-12-04 07:54:01 +0300

    styled

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote }from '../quote';
+import { Quote } from '../quote';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -7,14 +8,45 @@ import { Quote }from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  quotes:Quote [] = [
-    new  Quote (1,"Hammerton", "The best is yet to come",0,0),
-    new Quote (2,"Simon kimani", "Get ready for change",0,0),
-    new Quote (3,"Kelvin Mbugua","Change is inevitable",0,0),
-  ];
-  toggleDetails(index){
-    this.quotes[index].showDescription = !this.quotes[index].showDescription;
-  }
+     quotes:Quote [] = [
+      new  Quote (1,"Hammerton", "The best is yet to come",0,0),
+      new Quote (2,"Simon kimani", "Get ready for change",0,0),
+      new Quote (3,"Kelvin Mbugua","Change is inevitable",0,0),
+    ];
+
+    toggleDetails(index){
+      this.quotes[index].showDescription = !this.quotes[index].showDescription;
+    }
+
+    completeQuote(isComplete, index){
+      if (isComplete) {
+        this.quotes.splice(index,1);
+      }
+    }
+    numberOflikes:number=0;
+    numberOfDislikes:number=0;
+    upvote(index){
+      this.quotes[index].upvote+=1;
+    }
+    downvote(index){
+      this.quotes[index].downvote+=1;
+    }
+    deleteQuote(isComplete, index){
+      if (isComplete) {
+        let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+  
+        if (toDelete){
+          this.quotes.splice(index,1)
+        }
+      }
+    }
+    addNewQuote(quote){
+      let quoteLength = this.quotes.length;
+      quote.id = quoteLength+1;
+  
+      this.quotes.push(quote)
+    }
+
   constructor() { }
 
   ngOnInit() {
